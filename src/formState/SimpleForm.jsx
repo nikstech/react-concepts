be165eee74c975b112ce9d923edf3939aaa8ae0f
Niklesh <tiwaneniklesh9@gmail.com>
2023-09-20 17:09:12 +0000

    handled the form inputs

--- src/formState/SimpleForm.jsx
+++ src/formState/SimpleForm.jsx
@@ -1,14 +1,28 @@
+import { useState } from "react";
+
 export default function SimpleForm() {
+  const [inputData, setInputData] = useState({})
+  const [singleFormData, setSingleFormData] = useState()
+
+  const handleInput = (e) =>{
+    setInputData({...inputData,[e.target.name]:e.target.value})
+  }
+  const handleSubmit = (e) =>{
+    e.preventDefault()
+    setSingleFormData(inputData)
+    console.log(inputData.firstName)
+  }
     return (
       <div className="App">
        <h1>Simple Form</h1>
        <form action="">
-        <input type="text" name="firstName"/><br/>
-        <input type="text" name="lastName"/><br/>
-        <input type="text" name="email"/><br/>
-        <input type="text" name="password"/><br/>
-        <input type="submit" name="register"/><br/>
+        <input type="text" name="firstName" placeholder="First Name" onChange={handleInput}/><br/>
+        <input type="text" name="lastName" placeholder="Last Name" onChange={handleInput}/><br/>
+        <input type="text" name="email" placeholder="Email" onChange={handleInput}/><br/>
+        <input type="text" name="password" placeholder="Password" onChange={handleInput}/><br/>
+        <input type="submit" name="register" onClick={handleSubmit}/><br/>
        </form>
+       {/* {inputData.map(item=>item.firstName)} */}
       </div>
     );
   }
