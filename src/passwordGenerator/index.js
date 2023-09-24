944277f394d3a51ae694f7c88746fbbac0c56e4e
Niklesh <tiwaneniklesh9@gmail.com>
2023-09-24 10:59:43 +0000

    added pasword generater app

---
+++ src/passwordGenerator/index.js
@@ -0,0 +1,11 @@
+// import { useState } from "react";
+
+function PasswordGenerator() {
+//   const [color,setColor] = useState('green')
+  return (
+    <div className="App">
+        <h1>Nik</h1>
+    </div>
+  )}
+
+export default PasswordGenerator;
