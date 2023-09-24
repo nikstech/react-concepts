7f6381f265bc040263946cbf8dbba5fa3a102797
Niklesh <tiwaneniklesh9@gmail.com>
2023-09-24 15:34:22 +0000

    UI Done

--- src/passwordGenerator/index.js
+++ src/passwordGenerator/index.js
@@ -1,11 +1,60 @@
-// import { useState } from "react";
+import { useState } from "react";
 
 function PasswordGenerator() {
-//   const [color,setColor] = useState('green')
+    const [length,setLength] = useState('8')
+    const [numAllow,setNumAllow] = useState(true)
+    const [charAllow,setCharAllow] = useState(true)
+    const [password,setPassword] = useState('jh8#gj')
   return (
     <div className="App">
-        <h1>Nik</h1>
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
+        <h1 className='text-white text-center my-3'>Password generator</h1>
+        <div className="flex shadow rounded-lg overflow-hidden mb-4">
+          <input
+            type="text"
+            value={password}
+            className="outline-none w-full py-1 px-3"
+            placeholder="Password"
+            readOnly
+          />
+          <button
+            className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'
+          >copy</button>
+
+        </div>
+        <div className='flex text-sm gap-x-2'>
+          <div className='flex items-center gap-x-1'>
+            <input
+              type="range"
+              min={6}
+              max={100}
+              value={length}
+              className='cursor-pointer'
+            />
+            <label>Length: {length}</label>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              defaultChecked=''
+              id="numberInput"
+
+            />
+            <label htmlFor="numberInput">Numbers</label>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              defaultChecked=''
+              id="characterInput"
+
+            />
+            <label htmlFor="characterInput">Characters</label>
+          </div>
+        </div>
+      </div>
     </div>
-  )}
+  )
+}
 
 export default PasswordGenerator;
