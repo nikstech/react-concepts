c01afdc7564321334bf352ea85d2ae508dae9194
Niklesh <tiwaneniklesh9@gmail.com>
2023-09-25 17:22:17 +0000

    pasword generated

--- src/passwordGenerator/index.js
+++ src/passwordGenerator/index.js
@@ -1,10 +1,41 @@
-import { useState } from "react";
+import { useState, useEffect, useCallback, useRef } from "react";
 
 function PasswordGenerator() {
-    const [length,setLength] = useState('8')
-    const [numAllow,setNumAllow] = useState(true)
-    const [charAllow,setCharAllow] = useState(true)
-    const [password,setPassword] = useState('jh8#gj')
+  const [length, setLength] = useState('8')
+  const [numAllow, setNumAllow] = useState(false)
+  const [charAllow, setCharAllow] = useState(false)
+  const [password, setPassword] = useState('jh8#gj')
+
+  const passwordGenerate = useCallback(() => {
+    let pass = ''
+    let char = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+    let num = '0123456789'
+    let specialChar = '!@#$%^&*()_+="-"~`/?>.<,'
+
+    if (numAllow) { char += num }
+    if (charAllow) { char += specialChar }
+
+    for (let i = 0; i <= length; i++) {
+      let charIndex = Math.floor(Math.random() * char.length + 1)
+      pass += char.charAt(charIndex)
+    }
+    setPassword(pass)
+  }, [length, numAllow, charAllow])
+
+  const passRef = useRef()
+  const handleLength = (e) => {
+    setLength(e.target.value)
+  }
+  const handleNum = (e) =>{
+    setNumAllow(e.target.value)
+  }
+  const handleChar = (e) =>{
+    setCharAllow(e.target.value)
+  }
+  useEffect(() => {
+    passwordGenerate()
+  }, [length, numAllow, charAllow])
+
   return (
     <div className="App">
       <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
@@ -27,9 +58,10 @@ function PasswordGenerator() {
             <input
               type="range"
               min={6}
-              max={100}
+              max={30}
               value={length}
               className='cursor-pointer'
+              onChange={handleLength}
             />
             <label>Length: {length}</label>
           </div>
@@ -37,18 +69,16 @@ function PasswordGenerator() {
             <input
               type="checkbox"
               defaultChecked=''
-              id="numberInput"
-
-            />
+              onChange={handleNum}
+              id="numberInput" />
             <label htmlFor="numberInput">Numbers</label>
           </div>
           <div className="flex items-center gap-x-1">
             <input
               type="checkbox"
               defaultChecked=''
-              id="characterInput"
-
-            />
+              onChange={handleChar}
+              id="characterInput" />
             <label htmlFor="characterInput">Characters</label>
           </div>
         </div>
